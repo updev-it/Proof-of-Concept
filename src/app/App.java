468dbcaf986051c1f224140a6f08b9df9228b6a9
Bas van Wetten <bas.van.wetten@gmail.com>
2019-07-22 21:59:45 +0200

    Initial commit

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import com.thoughtworks.xstream.XStream;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
